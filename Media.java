281ae5830d0eea1f04312b99e70d4c7d80048038
pedrohvictoria <102485731+pedrohvictoria@users.noreply.github.com>
2025-12-19 23:28:41 -0500

    Update Media.java

--- Media.java
+++ Media.java
@@ -1,15 +1,12 @@
-//package StreamFlixUcc;
+//package StreamFlix;
 
 public abstract class Media {
-
     protected String title;
     protected String genre;
-    protected int duration;
 
-    public Media(String title, String genre, int duration) {
+    public Media(String title, String genre) {
         this.title = title;
         this.genre = genre;
-        this.duration = duration;
     }
 
     public abstract String getDetails();
