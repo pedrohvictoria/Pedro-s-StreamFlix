d5c80025212e2db365c4b969c5b03720585b13e6
pedrohvictoria <102485731+pedrohvictoria@users.noreply.github.com>
2025-12-19 23:29:24 -0500

    Update Series.java

--- Series.java
+++ Series.java
@@ -1,18 +1,21 @@
-//package StreamFlixUcc;
+//package StreamFlix;
 
 public class Series extends Media implements Playable {
+    private int episodes;
 
     public Series(String title, String genre, int episodes) {
-        super(title, genre, episodes);
+        super(title, genre);
+        this.episodes = episodes;
     }
 
     @Override
     public String getDetails() {
-        return "Series: " + title + " | Genre: " + genre + " | Episodes: " + duration;
+        return "Series: " + title + " | Genre: " + genre + " | " + episodes + " episodes";
     }
 
     @Override
     public void play() {
-        System.out.println("Now playing series: " + title);
+        System.out.println("Playing series: " + title + "...");
     }
 }
+
